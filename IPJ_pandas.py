d6faad02e45394249a82012617fd4ab234e8697d
memes-lab <lukas.dorda72@gmail.com>
2023-11-20 17:53:24 +0100

    alles mit plotly Erzegungsprognose

--- IPJ_pandas.py
+++ IPJ_pandas.py
@@ -3,18 +3,14 @@ import matplotlib.pyplot as plt
 from datetime import datetime
 import time
 import numpy as np
+import plotly.graph_objects as go
+
 
 # Interaktiver Benutzereingabe für das Datum
 selected_date_str = input("Bitte geben Sie das Datum im Format TT.MM.JJJJ ein: ")
 selected_date = datetime.strptime(selected_date_str, "%d.%m.%Y")
 
 start_time = time.time()                      #Startzeit des Programms
-
-
-# Pfad der Dateien
-# CSV-Dateien müssen im gleichen Ordner wie das Python-Skript liegen
-import os
-print(os.getcwd())
 
 # Dateinamen
 file_production = 'Realisierte_Erzeugung_202001010000_202212312359_Viertelstunde.csv'
@@ -82,40 +78,6 @@ for year, data in data_by_year.items():             # Ausgabe der aggregierten D
 total_renewable_production = production_df[columns_to_clean].sum(axis=1)
 total_consumption = consumption_df[CONSUMPTION]
 
-
-def range1(array1, array2):               # Berechnung der prozentualen Anteile der erneuerbaren Energieerzeugung am Gesamtverbrauch
-    if len(array1) != len(array2):
-        raise ValueError("Arrays must be the same length")
-    
-    counts = [0] * 111
-
-    for val1, val2 in zip(array1, array2):
-        ratio = val1 / val2
-        percent = int(ratio * 100)
-
-        if percent == 100:
-            counts[percent] += 1
-        elif 0 <= percent < 110:
-            counts[percent] += 1
-
-    return counts
-
-counts = range1(total_renewable_production, total_consumption)
-n = range(111) # Anzahl der Prozenten
-
-# Ausgabe von Anteilen
-def get_result(array1, array2):
-    print("Anteile in %:")
-    if len(array1) != len(array2):
-        raise ValueError("Arrays must be the same length")
-    
-    for val1, val2 in zip(array1, array2):
-        print( val1, "% :"   , val2)
-
-get_result(n, counts)
-print("Anzahl der Viertelstunden in 3 Jahren:", sum(counts))
-print()
-
 # Filtern der Daten für das ausgewählte Datum
 selected_production = production_df[production_df[DATE] == selected_date]
 selected_consumption = consumption_df[consumption_df[DATE] == selected_date]
@@ -143,7 +105,23 @@ plt.gca().set_xticklabels(selected_production[STARTTIME].dt.strftime('%H')[::4])
 
 plt.show()
 
+# Berechnung der prozentualen Anteile der erneuerbaren Energieerzeugung am Gesamtverbrauch
+percent_renewable = total_renewable_production / total_consumption * 100 
 
+counts, intervals = np.histogram(percent_renewable, bins = np.arange(0, 111, 1))  # Use NumPy to calculate the histogram of the percentage distribution
+
+x = intervals[:-1]          # Define the x-axis values as the bin edges
+labels = [f'{i}%' for i in range(0, 111, 1)] # Create labels for x-axis ticks (von 0 bis 111 in Einzelnschritten)
+
+fig = go.Figure(data=[go.Bar(x=x, y=counts)])    # Create a bar chart using Plotly
+fig.update_layout(xaxis=dict(tickmode='array', tickvals=list(range(0, 111, 5)), ticktext=labels[::5]))  # X-axis label settings
+
+# Title and axis labels settings
+fig.update_layout(title='Anzahl der Viertelstunden in Jahren 2020-2022 mit 0-110 % EE-Anteil',
+                  xaxis_title='Prozentsatz erneuerbarer Energie',
+                  yaxis_title='Anzahl der Viertelstunden')
+
+fig.show()
 
 """
 # Ein Beispiel für die Berechnung der Gesamtsumme einer bestimmten Art
@@ -170,7 +148,14 @@ print(f"{selected_energy_type} Production List for {selected_date.year}: {select
 
 """
 
-# function that multiplies Windonshore, Windoffshore and PV  with 2030 "Ausbaufaktoren"
+# code to make 2030 prediction
+# 2030 prediction
+
+
+# Define the factors
+windonshore_2030_factor = 2.03563  # assuming Wind Onshore will increase by 203%
+windoffshore_2030_factor = 3.76979  # assuming Wind Offshore will 376% increase
+pv_2030_factor = 3.5593  # assuming PV will increase by 350%
 
 def scale_2030_factors(df, windonshore_factor, windoffshore_factor, pv_factor):
     df_copy = df.copy()
@@ -181,23 +166,19 @@ def scale_2030_factors(df, windonshore_factor, windoffshore_factor, pv_factor):
     return df_copy
 
 
-# Define the factors
-windonshore_2030_factor = 2.03563  # assuming Wind Onshore will increase by 203%
-windoffshore_2030_factor = 3.76979  # assuming Wind Offshore will 376% increase
-pv_2030_factor = 3.5593  # assuming PV will increase by 350%
-
 # Scale the data by the factors
 scaled_production_df = scale_2030_factors(production_df, windonshore_2030_factor, windoffshore_2030_factor, pv_2030_factor)
 
-# Filter scaled data for the selected date
+# Filter the data for the selected date
 scaled_selected_production = scaled_production_df[scaled_production_df[DATE] == selected_date]
 
+# Plot the data
 # Plotting
 plt.figure(figsize=(12, 6))
-plt.plot(scaled_selected_production[STARTTIME], scaled_selected_production['Total Production'], label='Total Renewable Production - Scaled')
+plt.plot(scaled_selected_production[STARTTIME], scaled_selected_production['Total Production'], label='Total Renewable Production')
 plt.plot(selected_consumption[STARTTIME], selected_consumption[CONSUMPTION], label='Total Consumption')
 
-plt.title(f'Scaled Renewable Energy Production and Total Consumption on {selected_date_str}')
+plt.title(f'Renewable Energy Production and Total Consumption on {selected_date_str}')
 plt.xlabel('Time (hours)')
 plt.ylabel('Energy (MWh)')
 plt.legend()
@@ -205,55 +186,29 @@ plt.grid(True)
 
 # Format x-axis ticks and labels
 unique_hours = sorted(scaled_selected_production[STARTTIME].dt.hour.unique())
-plt.xticks(scaled_selected_production[STARTTIME], scaled_selected_production[STARTTIME].dt.strftime('%H:%M'), rotation=45)
+plt.xticks(scaled_selected_production[STARTTIME], selected_production[STARTTIME].dt.strftime('%H:%M'), rotation=45)
 plt.gca().set_xticks(scaled_selected_production[STARTTIME][::4])
 plt.gca().set_xticklabels(scaled_selected_production[STARTTIME].dt.strftime('%H')[::4])
-
 plt.show()
 
 
-counts = range1(total_renewable_production, total_consumption)
-n = range(111) # Anzahl der Prozenten
-# Ausgabe von Anteilen
-def get_result(array1, array2):
-    print("Anteile in %:")
-    if len(array1) != len(array2):
-        raise ValueError("Arrays must be the same length")
-    
-    for val1, val2 in zip(array1, array2):
-        print( val1, "% :"   , val2)
+#code to do 2030 quarter hours
+total_scaled_renewable_production = scaled_production_df[columns_to_clean].sum(axis=1)
 
-get_result(n, counts)
-print("Anzahl der Viertelstunden in 2030 (prognostiziert):", sum(counts))
-print()
+# Berechnung der prozentualen Anteile der erneuerbaren Energieerzeugung am Gesamtverbrauch
+percent_renewable = total_scaled_renewable_production / total_consumption * 100 
 
-# Calculate total renewable production for scaled data
-total_renewable_scaled_production = scaled_production_df[columns_to_clean].sum(axis=1)
+counts, intervals = np.histogram(percent_renewable, bins = np.arange(0, 111, 1))  # Use NumPy to calculate the histogram of the percentage distribution
 
-# get the counts for scaled data
-scaled_counts = range1(total_renewable_scaled_production, total_consumption)
+x = intervals[:-1]          # Define the x-axis values as the bin edges
+labels = [f'{i}%' for i in range(0, 111, 1)] # Create labels for x-axis ticks (von 0 bis 111 in Einzelnschritten)
 
-# Ausgabe von Anteilen for scaled data
-get_result(n, scaled_counts)
-print("Anzahl der Viertelstunden in 2030 (mit erreichen der 2030 Ausbauzielen des BMWK):", sum(scaled_counts))
-print()
+fig = go.Figure(data=[go.Bar(x=x, y=counts)])    # Create a bar chart using Plotly
+fig.update_layout(xaxis=dict(tickmode='array', tickvals=list(range(0, 111, 5)), ticktext=labels[::5]))  # X-axis label settings
 
-# Generate the plot for scaled data
-fig, ax = plt.subplots(figsize=(10, 6))  # Adjust size as needed
-x = range(111)  # For percentages 0-110
-ax.bar(x, scaled_counts, width=1)  # width=1 for contiguous bars
-ax.set_title('Anzahl der Viertelstunden mit 1-100 % EE-Anteil in 2030 (prognostiziert)')
-ax.set_xlabel('Percentage of Total Consumption (%)')
-ax.set_ylabel('Anzahl der Viertelstunden')
-ax.set_xticks(x[::10])
-ax.set_xticklabels([f'{i}%' for i in range(0, 111, 10)])
-plt.show()
+# Title and axis labels settings
+fig.update_layout(title='Anzahl der Viertelstunden in Jahren 2020-2022 mit 0-110 % EE-Anteil',
+                  xaxis_title='Prozentsatz erneuerbarer Energie',
+                  yaxis_title='Anzahl der Viertelstunden')
 
-# assuming quarter_hour_scaled_production and quarter_hour_consumption are your quarter-hourly production and consumption data
-# Calculate the ratio for each quarter-hour
-ratio_quarter_hour = quarter_hour_scaled_production / quarter_hour_consumption
-
-# Check if any ratio is greater than 1.11 (111%)
-higher_share_quarter_hour = any(ratio_quarter_hour > 1.11)
-
-print("The renewables have a higher share than 111% in any quarter-hour:", higher_share_quarter_hour)
+fig.show()
